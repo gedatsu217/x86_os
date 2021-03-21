d734802869dc677006e4feffd7ae7f0e3cd5ff3c
gedatsu217 <zais712@yahoo.co.jp>
2021-03-21 15:57:44 +0900

    done font

---
+++ src/modules/real/get_font_adr.s
@@ -0,0 +1,29 @@
+get_font_adr:
+    push    bp
+    mov     bp, sp
+
+    push    ax
+    push    bx
+    push    si
+    push    es
+    push    bp
+
+    mov     si, [bp + 4]
+
+    mov     ax, 0x1130
+    mov     bh, 0x06
+    int     10h
+
+    mov     [si + 0], es
+    mov     [si + 2], bp
+
+    pop     bp
+    pop     es
+    pop     si
+    pop     bx
+    pop     ax
+
+    mov     sp, bp
+    pop     bp
+
+    ret
